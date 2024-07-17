f184563e8ade9a916395e5459e4315acebe22759
Horacio E. Valenzuela <valenzuelahoracioe@gmail.com>
2024-07-17 15:28:14 -0300

    Change from Java to kotlin

---
+++ src/main/kotlin/com/example/login_register_demo/LoginRegisterDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.login_register_demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LoginRegisterDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<LoginRegisterDemoApplication>(*args)
+}
